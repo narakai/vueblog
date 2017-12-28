95deb0acbfffbfb6086cf1f1cf6b0ec72c340df8
laileon <leon@igola.com>
2017-12-28 18:08:50 +0800

    port

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 // Template version: 1.2.7
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   dev: {
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/': {
-        target: 'http://localhost:8081',
+        target: 'http://localhost:80',
         changeOrigin: true,
         pathRewrite: {
           '^/': ''
@@ -79,4 +79,4 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   }
-}
+};
